d4440980381cb87c05a59fa8fe9484c01b39ae8a
AlgorithmEnigma <wraithbournejl@gmail.com>
2023-02-17 21:50:22 -0500

    Dynamic speech bubble

--- speech.py
+++ speech.py
@@ -1,18 +1,19 @@
 import textwrap
 
-def speech_bubble(text):
-    top_line="_"*35 # Creates a line 35 dashes long
-    print("\n", top_line.rjust(52)) # Prints the line justified to the right
-    print("/".rjust(18), "\ ".rjust(36)) # Prints the first line of slashes
-    if len(text) < 30:
-        print('|'.rjust(17), text.rjust(5 + len(text)), '|'.rjust(33 - len(text)), end="\n", sep="")
-        print('\\'.rjust(18), ('_'*32).rjust(33), '/', end="")
-    else:
-        wrapper = textwrap.TextWrapper(width=30)
-        word_list = wrapper.wrap(text=text)
-        for i in range(len(word_list)):
-            if i == 0:
-                print('/'.rjust(17), word_list[i].rjust(2 + len(word_list[i])), '\\'.rjust(34 - len(word_list[i])))
-            if i == 1:
-                print('\\'.rjust(17), word_list[i].rjust(2 + len(word_list[i])), '/'.rjust(34 - len(word_list[i])), end="")
 
+def speech_bubble(text: str):
+    max_length = 35
+
+    wrapped_text = []
+    split_text = text.split(", ")
+    for i in split_text:
+        wrapped_text.append(textwrap.wrap(i, max_length))
+    # print(wrapped_text)
+
+    top_line = "_" * max_length
+    print("\n", top_line.rjust(52))
+    print("/".rjust(18), "\ ".rjust(37), sep="", end="\n")
+    for i in wrapped_text:
+        print("|".rjust(17), "    ", i[0], "|".rjust(34 - len(i[0])), sep="")
+
+    print("\\".rjust(18), ("_" * (max_length - 2)).rjust(35), "/", sep="", end="")
